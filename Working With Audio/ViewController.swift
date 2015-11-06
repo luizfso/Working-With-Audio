774cc7bd7cc41a4fa73ec9c7c80a213576a1e7f9
Luiz Fernando Santiago <luizfso@gmail.com>
2015-11-06 14:05:36 -0200

    My Class for working with Audio

--- Working With Audio/ViewController.swift
+++ Working With Audio/ViewController.swift
@@ -7,12 +7,74 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    @IBAction func adjustVolume(sender: AnyObject) {
+        
+        player.volume = volumeSlider.value
+        
+    }
+    
+    @IBOutlet weak var scrubSlider: UISlider!
+    
+    @IBAction func scrub(sender: AnyObject) {
+        
+        player.currentTime = NSTimeInterval(scrubSlider.value)
+        
+        print(scrubSlider.value)
+    }
+    
+    @IBAction func play(sender: AnyObject) {
+        player.play()
+    }
+    
+    @IBAction func pause(sender: AnyObject) {
+        player.pause()
+    }
+    
+    
+    func updateScrubSlider(){
+    
+        scrubSlider.value = Float(player.currentTime)
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+        
+        player.pause()
+        
+        do{
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("bach", ofType: "mp3")!))
+        } catch{
+            print("Error")
+        }
+        
+    }
+    
+    
+    var player: AVAudioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        do{
+            
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("bach", ofType: "mp3")!))
+            
+             scrubSlider.maximumValue = Float(player.duration)
+            
+        } catch{
+            
+            print("Error")
+        }
+        
+        _ = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateScrubSlider"), userInfo: nil, repeats: true)
+        
+
     }
 
     override func didReceiveMemoryWarning() {
